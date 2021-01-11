7f40d6dfe4c2efc56af0890871ccde126b78b273
Hemanth Babu Muddam <19pa1a05b3@vishnu.edu.in>
2021-01-11 15:40:36 +0530

    update TwoSum

--- Practice Problems/Hash Table/TwoSum.java
+++ Practice Problems/Hash Table/TwoSum.java
@@ -1,7 +1,7 @@
 class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i <= nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if(map.containsKey(target - nums[i])) {
                 return new int[] {map.get(target - nums[i]), i};
             } else {
